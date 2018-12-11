1a963b055e9fdb9ee8aeeed74043e161119af7f0
tomitake <tomitake.yokosekanemoto@gmail.com>
2018-12-11 19:18:23 +0900

    Divide models.py

---
+++ opac/models/masters/__init__.py
@@ -0,0 +1,6 @@
+from opac.models.masters.author import Author
+from opac.models.masters.book import Book
+from opac.models.masters.library import Library
+from opac.models.masters.publisher import Publisher
+from opac.models.masters.stock import Stock
+from opac.models.masters.translator import Translator
---
+++ opac/models/transactions/__init__.py
@@ -0,0 +1,4 @@
+from opac.models.transactions.holding import Holding
+from opac.models.transactions.lending import Lending
+from opac.models.transactions.renewing import Renewing
+from opac.models.transactions.reservation import Reservation
