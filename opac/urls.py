2aba081825ac57388c89ca04774fae5204752b0a
tomitake <tomitake.yokosekanemoto@gmail.com>
2018-12-14 22:53:45 +0900

    Change BookDetailView to StockListView

--- opac/urls.py
+++ opac/urls.py
@@ -7,5 +7,5 @@ app_name = 'opac'
 urlpatterns = [
     path('search/', views.SearchView.as_view(), name='search'),
     path('book/<int:book_id>/stocks/',
-         views.BookDetailView.as_view(), name='stock_list'),
+         views.StockListView.as_view(), name='stock_list'),
 ]
