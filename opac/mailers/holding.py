ba6a7200b8e776c97061df1cee7b43381faf007b
tomitake <tomitake.yokosekanemoto@gmail.com>
2018-12-22 22:12:09 +0900

    Use f-string

--- opac/mailers/holding.py
+++ opac/mailers/holding.py
@@ -14,7 +14,10 @@ class HoldingCreatedMailer:
         対象の取置
     """
     def __init__(self, holding):
-        self._holding = holding
+        self._name = holding.user.username
+        self._book = holding.stock.book
+        self._expiration_date = holding.expiration_date
+        self._email = holding.user.email
 
     def exec(self):
         """メールを送信する。
@@ -27,13 +30,11 @@ class HoldingCreatedMailer:
         try:
             send_mail(
                 '蔵書取り置きのご連絡 ○○○図書館',
-                '{} さんが予約されていた {} を取り置きしました。\n取置期限は{}です。'.format(
-                    self._holding.user.username,
-                    self._holding.stock.book,
-                    self._holding.expiration_date
-                ),
+                f'''{self._name} さんが予約されていた {self._book} を取り置きしました。
+                取置期限は{self._expiration_date}です。
+                ''',
                 'from@django-opac.com',
-                [self._holding.user.email],
+                [self._email],
                 fail_silently=False,
             )
         except SMTPException as e:
